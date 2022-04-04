0f1341bc69bb71345d6c4a0cbcdc352969f80eea
DaiWang <1403693160@qq.com>
2022-04-04 09:33:44 +0800

    一键三连

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          点赞，投币，收藏
         </p>
         <a
           className="App-link"
